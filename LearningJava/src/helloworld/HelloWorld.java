2fb1ec7ca074e9bfb5491184c4d935685818096b
laeti <laeti@10.0.0.21>
2025-10-19 22:23:09 +0200

    push

--- LearningJava/src/helloworld/HelloWorld.java
+++ LearningJava/src/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("I am learning Java");
+        System.out.println("I am learning Java!");
     }
 }
